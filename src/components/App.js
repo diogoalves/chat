3f3564c01a6950d1228378e730f9cc5be84d0c8c
casa <casa@casas-iMac.local>
2018-07-14 22:50:15 -0300

    feat: adds main component with material-ui theme init

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import TopBar from './TopBar';
+import Conversation from './Conversation';
+import Input from './Input';
+
+import withRoot from './withRoot';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TopBar />
+        <Conversation />
+        <Input />
+      </div>
+    );
+  }
+}
+
+export default withRoot(App);
