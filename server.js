a651b1c7b75eef304e2c404cf02163b365dddb9d
casa <casa@diogo.local>
2018-07-17 12:04:46 -0300

    chore: add now deploy script

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var io = require('socket.io')(http);
 const path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/build/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/')));
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 io.on('connection', function(socket){
