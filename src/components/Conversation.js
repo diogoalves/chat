fd7533764fc53c36746ad2686686028c0848105d
casa <casa@casas-iMac.local>
2018-07-16 23:05:11 -0300

    feat: make conversation scroll bottom on update

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -12,21 +12,34 @@ const styles = theme => ({
   }
 });
 
-function Conversation(props) {
-  const { messages = [], classes } = props;
+class Conversation extends Component {
+  scrollToBottom = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  };
 
-  return (
-    <Paper className={classes.root} elevation={1}>
-      {messages.map((e, i) => (
-        <MessageItem
-          key={i}
-          time={new Date(e.time).toLocaleString()}
-          author={e.author}
-          content={e.content}
-        />
-      ))}
-    </Paper>
-  );
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render() {
+    const { messages = [], classes } = this.props;
+    return (
+      <Paper className={classes.root} elevation={1}>
+        {messages.map((e, i) => (
+          <MessageItem
+            key={i}
+            time={new Date(e.time).toLocaleString()}
+            author={e.author}
+            content={e.content}
+          />
+        ))}
+      </Paper>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
